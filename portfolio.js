77bfff69924ec18d1e1bee391565eabad5b8ab10
briahnayee <briahnayee@gmail.com>
2020-08-30 22:07:40 -0700

    homepage mobile responsive

---
+++ portfolio.js
@@ -0,0 +1,54 @@
+<body>
+    <h1>Projects</h1>
+
+    <div class="project">
+        <h3>Audition Tracker</h3>
+        <img src="https://i.imgur.com/qMRr5zT.png" alt="Audition Tracker" width="250">
+            <p>An easier way for actors to track what auditions they've been on and which casting directors they have met.
+    </p>
+            <p>Technologies used: React, PostgreSQL, Node, HTML, CSS, JavaScript</p>
+            <div class="buttons">
+                <a href="https://github.com/briahnayee/audition_tracker" class="button">Github Link</a>
+                <a href="https://audition-tracker.herokuapp.com/" class="button">Try the app</a>
+            </div>
+</div>
+
+        <div class="project">
+            <h3>Dungeons, Daddies, and Divas</h3>
+            <img src="https://i.imgur.com/C0JqsvF.png" alt="Dungeons, Daddies, and Divas" width="250">
+                <p>A dating and friendship networking app for fans of the table-top game Dungeons and Dragons.</p>
+                <p>Technologies used: Python, Django, PostgreSQL, CSS, HTML</p>
+                <div class="buttons">
+                    <a href="https://github.com/briahnayee/dungeons_daddies_and_divas" class="button">Github Link</a>
+                    <a href="https://dungeons-daddies-and-divas.herokuapp.com/" class="button">Try the app</a>
+                </div>
+</div>
+
+            <div class="project">
+                <h3>Scrapbook</h3>
+                <img src="https://i.imgur.com/guGvN0i.png" alt="Scrapbook" width="250">
+                    <p>Scrapbook is a social media site designed to allow photographers, models, and other creatives to share their
+        work.</p>
+                    <p>Technologies used: Mongoose, MongoDB, Express, Node, OAuth, HTML, CSS, JavaScript</p>
+                    <div class="buttons">
+                        <a href="https://github.com/briahnayee/Scrapbook" class="button">Github Link</a>
+                        <a href="https://seiscrapbook.herokuapp.com/home" class="button">Try the app</a>
+                    </div>
+</div>
+
+                <div class="project">
+                    <h3>Simon Game</h3>
+                    <img src="https://i.imgur.com/tVI3rYZ.png" alt="Simon Game" width="250">
+                        <p>This is based off of the classic Simon game where you repeat the colors.</p>
+                        <p>Technologies used: HTML, CSS, JavaScript</p>
+                        <div class="buttons">
+                            <a href="https://github.com/briahnayee/Simon_Says" class="button">Github Link</a>
+                            <a href="https://briahnayee.github.io/Simon_Says/" class="button">Try the app</a>
+                        </div>
+</div>
+
+                    <div>
+                        <h2>Contact</h2>
+                        <a class="email" href="mailto: briahnayee@gmail.com">briahnayee@gmail.com</a>
+                    </div>
+</body>
